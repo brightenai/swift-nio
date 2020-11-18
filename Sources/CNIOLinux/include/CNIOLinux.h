05a1edc96300b5ccd15a891b017af74232dbefc3
John Burkey <johnburkey@brighten.ai>
2020-11-18 12:50:05 -0600

    d

--- Sources/CNIOLinux/include/CNIOLinux.h
+++ Sources/CNIOLinux/include/CNIOLinux.h
@@ -16,6 +16,10 @@
 
 #include "ifaddrs-android.h"
 
+#ifdef __ANDROID__
+#define (__force __poll_t)
+#endif
+
 #ifdef __linux__
 
 #include <sys/epoll.h>
@@ -30,22 +34,22 @@
 
 #ifdef __ANDROID__
 
-#define EPOLLIN  0x00000001
-#define EPOLLPRI  0x00000002
-#define EPOLLOUT  0x00000004
-#define EPOLLERR  0x00000008
-#define EPOLLHUP  0x00000010
-#define EPOLLNVAL  0x00000020
-#define EPOLLRDNORM  0x00000040
-#define EPOLLRDBAND  0x00000080
-#define EPOLLWRNORM  0x00000100
-#define EPOLLWRBAND  0x00000200
-#define EPOLLMSG  0x00000400
-#define EPOLLRDHUP  0x00002000
-#define EPOLLEXCLUSIVE ( (1U << 28))
-#define EPOLLWAKEUP ( (1U << 29))
-#define EPOLLONESHOT ( (1U << 30))
-#define EPOLLET  (1U << 31)
+//#define EPOLLIN  0x00000001
+//#define EPOLLPRI  0x00000002
+//#define EPOLLOUT  0x00000004
+//#define EPOLLERR  0x00000008
+//#define EPOLLHUP  0x00000010
+//#define EPOLLNVAL  0x00000020
+//#define EPOLLRDNORM  0x00000040
+//#define EPOLLRDBAND  0x00000080
+//#define EPOLLWRNORM  0x00000100
+//#define EPOLLWRBAND  0x00000200
+//#define EPOLLMSG  0x00000400
+//#define EPOLLRDHUP  0x00002000
+//#define EPOLLEXCLUSIVE ( (1U << 28))
+//#define EPOLLWAKEUP ( (1U << 29))
+//#define EPOLLONESHOT ( (1U << 30))
+//#define EPOLLET  (1U << 31)
 
 /** The opaque structure implementing `FILE`. Do not make any assumptions about its content. */
 struct __sFILE {
